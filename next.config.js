e1f4d4c7e83b195ff0a29d4fb6b1bc0d0e578b1a
Brian Anglin <brianranglin@gmail.com>
2023-04-05 12:58:45 -0400

    Inital commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => ({
+    fallback: [
+      {
+        source: "/:path*",
+        destination: `https://latch-creative-co.webflow.io/:path*`,
+      },
+    ]
+  })
 }
 
 module.exports = nextConfig
